e101d8315577db4736e55b6efb94dce54e4e48ec
Sky Winston <skywinston@gmail.com>
2015-11-06 13:34:14 -0700

    I have the form showing for new posts when the new post button is clicked

--- exercises/reddit-clone/public/javascripts/postsController.js
+++ exercises/reddit-clone/public/javascripts/postsController.js
@@ -4,7 +4,16 @@ app.controller('postsController', function($scope, $http){
   $http.get('/api/1/posts').then(function(response){
     $scope.posts = response.data;
   });
-  
+
+  $scope.newPost = {
+    visible: false
+  };
+
   $scope.posts = [];
+
+  $scope.newPostForm = function(){
+    $scope.newPost.visible = true;
+    document.getElementById('title').focus();
+  }
 
 });
